c37ed67268551a81966ac89318e99084680ee87e
Indigo <iredward27@gmail.com>
2021-09-03 10:59:16 -0400

    Added more code for making frass data usable

--- scripts/data_manipulation.R
+++ scripts/data_manipulation.R
@@ -1,14 +1,146 @@
 
 source('scripts/session_setup.R')
 
-# add Julian week to frassData
 
-frassDataJulian <- 
+# frass -------------------------------------------------------------------
+
+
+# generate modified frass data that is usable for plotting
+
+usefulFrass <- 
   frassData %>% 
+  # add a julian week column
   mutate(
     Julian.Week = floor(
       julian(
         as.Date(
           frassData$Date.Set,
           format = '%m/%d/%Y'),
-        origin = as.Date('2021-01-01')) / 7))
+        origin = as.Date('2021-01-01')) / 7)) %>% 
+  # add a year column
+  mutate(
+    Year = year(
+      as.Date(frassData$Date.Set,
+              format = '%m/%d/%Y'))) %>% 
+  # remove rows where mass or number of frass is NA
+  filter(
+    !is.na(Frass.mass..mg.),
+    !is.na(Frass.number)) %>% 
+  # join reliability data
+  
+  
+# need to modify data for:
+# make site code vs site name match across CC, frass, and frass reliability to be able to join - probably need to pull site keys from another repo
+# do not include if '90 degree' in the notes column - indicates potential false 0 - and do we want to include accuracy sorting for 'tilt' in that column?
+# join reliability to main file - include option in density by week function for sorting by reliability
+
+
+# CC! ---------------------------------------------------------------------
+
+
+# Function for calculating the mode of a series of values
+# --in this particular use case, if there multiple modes, we want the largest value
+Mode = function(x){ 
+  if (!is.numeric(x)) {
+    stop("values must be numeric for mode calculation")
+  }
+  ta = table(x)
+  tam = max(ta)
+  mod = as.numeric(names(ta)[ta == tam])
+  return(max(mod))
+}
+
+
+# Function for substituting values based on a condition using dplyr::mutate
+# Modification of dplyr's mutate function that only acts on the rows meeting a condition
+mutate_cond <- function(.data, condition, ..., envir = parent.frame()) {
+  condition <- eval(substitute(condition), .data, envir)
+  .data[condition, ] <- .data[condition, ] %>% mutate(...)
+  .data
+}
+
+
+#function for calculating and displaying mean density by week for CC data
+
+meanDensityByWeek = function(surveyData,
+                             # merged dataframe of Survey and arthropodSighting tables for a single site
+                             ordersToInclude = 'All',
+                             # which arthropod orders to calculate density for (codes)
+                             minLength = 0,
+                             # minimum arthropod size to include 
+                             jdRange = c(1,365),
+                             outlierCount = 10000,
+                             plot = FALSE,
+                             plotVar = 'fracSurveys', 
+                             # 'meanDensity' or 'fracSurveys' or 'meanBiomass'
+                             minSurveyCoverage = 0.8,
+                             # minimum proportion of unique survey branches examined per week in order to include the week as a data point
+                             allDates = TRUE,
+                             new = TRUE,
+                             color = 'black',
+                             allCats = TRUE,
+                             ...) 
+{
+  if(length(ordersToInclude)==1 & ordersToInclude[1]=='All') {
+    ordersToInclude = unique(surveyData$Group)
+  }
+  
+  numUniqueBranches = length(unique(surveyData$PlantFK))
+  
+  firstFilter = surveyData %>%
+    filter(julianday >= jdRange[1], julianday <= jdRange[2]) %>%
+    mutate(julianweek = 7*floor(julianday/7) + 4)
+  
+  effortByWeek = firstFilter %>%
+    group_by(julianweek) %>%
+    summarize(nSurveyBranches = n_distinct(PlantFK),
+              nSurveys = n_distinct(ID)) %>%
+    mutate(modalBranchesSurveyed = Mode(5*ceiling(nSurveyBranches/5)),
+           nSurveySets = nSurveys/modalBranchesSurveyed,
+           modalSurveySets = Mode(round(nSurveySets)),
+           okWeek = ifelse(nSurveySets/modalSurveySets >= minSurveyCoverage, 1, 0))
+  
+  if (allDates) {
+    effortByWeek$okWeek = 1
+  }
+  
+  if (!allCats) {
+    secondFilter = firstFilter %>%
+      filter(Hairy != 1, Tented != 1, Rolled != 1)
+  } else {
+    secondFilter = firstFilter
+  }
+  
+  arthCount = secondFilter %>%
+    filter(Length >= minLength, 
+           Group %in% ordersToInclude) %>%
+    mutate(Quantity2 = ifelse(Quantity > outlierCount, 1, Quantity)) %>% #outlier counts replaced with 1
+    group_by(julianweek) %>%
+    summarize(totalCount = sum(Quantity2, na.rm = TRUE),
+              numSurveysGTzero = length(unique(ID[Quantity > 0])),
+              totalBiomass = sum(Biomass_mg, na.rm = TRUE)) %>% 
+    right_join(effortByWeek, by = 'julianweek') %>%
+    filter(okWeek == 1) %>%
+    #next line replaces 3 fields with 0 if the totalCount is NA
+    mutate_cond(is.na(totalCount), totalCount = 0, numSurveysGTzero = 0, totalBiomass = 0) %>%
+    mutate(meanDensity = totalCount/nSurveys,
+           fracSurveys = 100*numSurveysGTzero/nSurveys,
+           meanBiomass = totalBiomass/nSurveys) %>%
+    arrange(julianweek) %>%
+    data.frame()
+  
+  if (plot & new) {
+    plot(arthCount$julianweek, arthCount[, plotVar], type = 'l', 
+         col = color, las = 1, ...)
+    points(arthCount$julianweek, arthCount[, plotVar], pch = 16, col = color, ...)
+  } else if (plot & new==F) {
+    points(arthCount$julianweek, arthCount[, plotVar], type = 'l', col = color, ...)
+    points(arthCount$julianweek, arthCount[, plotVar], pch = 16, col = color, ...)
+  }
+  return(arthCount)
+}
+
+
+# data comparison ---------------------------------------------------------
+
+# join data by julian week for plotting
--- scripts/session_setup.R
+++ scripts/session_setup.R
@@ -10,3 +10,16 @@ library(tidyverse)
 frassData <- read_csv('data/raw_data/frass_2021-08-30.csv')
 
 ccData <- read_csv('data/raw_data/CC_2021-08-10.csv')
+
+# download frass reliability data - not necessary unless outdated
+
+#gsheet2tbl('https://docs.google.com/spreadsheets/d/1RwXzwhHUbP0m5gKSOVhnKZbS1C_NrbdfHLglIVCzyFc/edit#gid=1611171427') %>% 
+#  write.csv(paste('data/raw_data/frass_reliability_',
+#                  Sys.Date(),
+#                  '.csv',
+#                  sep = ''))
+
+
+# read in frass reliability data
+
+frassReliability <- read_csv('data/raw_data/frass_reliability_2021-09-03.csv')
