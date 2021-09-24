680d57c8ff0d02003d2ed92348419da3c1ca0bd7
Indigo <iredward27@gmail.com>
2021-09-24 09:48:58 -0400

    created basic version of ggplot with both variables

--- scripts/data_comparison.R
+++ scripts/data_comparison.R
@@ -1,13 +1,13 @@
 
 # setup -------------------------------------------------------------------
 
-# load libraries
+# source scripts
 
-library(tidyverse)
+source('scripts/functions.R')
 
-library(ggplot)
+# load packages
 
-library(lubridate)
+library(ggplot2)
 
 # read in data
 
@@ -23,6 +23,41 @@ comp_data_r3 <-
   read_csv(
     'data/processed_data/weekly_stats_2018-2021_rel3.csv')
 
+
+# plot data together ------------------------------------------------------
+
+ggplot(
+  data = comp_data_r3 %>% 
+    filter(
+      year == 2021,
+      site == 'NC Botanical Garden') %>% 
+    drop_na(mean_mass),
+  mapping = aes(
+    x = julianweek,
+    y = mean_mass)) +
+  geom_point(color = 'forestgreen') +
+  geom_line(color = 'forestgreen') +
+  scale_y_continuous(
+    name = 'Mean Frass Mass',
+    sec.axis = sec_axis(~./5, name = 'Mean Caterpillar Biomass')) +
+  geom_point(
+    data = comp_data_r3%>% 
+      filter(
+        year == 2021,
+        site == 'NC Botanical Garden'),
+    mapping = aes(
+      x = julianweek,
+      y = meanBiomass*5),
+    color = 'blue') +
+  geom_line(
+    data = comp_data_r3%>% 
+      filter(
+        year == 2021,
+        site == 'NC Botanical Garden'),
+    mapping = aes(
+      x = julianweek,
+      y = meanBiomass*5),
+    color = 'blue')
 
 # next steps ------------------------------------
 
