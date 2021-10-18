ab56e54fc69248c1dee9e781e4426e532215af0d
Indigo <iredward27@gmail.com>
2021-10-18 14:42:35 -0400

    more lag analysis

--- scripts/lag_comparison.R
+++ scripts/lag_comparison.R
@@ -7,22 +7,83 @@ source('scripts/functions.R')
 
 comp_data <- read_csv('data/processed_data/weekly_stats_2015-2021_rel2.csv')
 
-comp_data %>% 
-  cbind(
-    map_dfc(
-      1:3,
-      ~ lead(comp_data$mean_number, n = .)) %>% set_names(c('nlag1', 'nlag2', 'nlag3'))) %>% 
-  cbind(
-    map_dfc(
-      1:3,
-      ~ lag(comp_data$mean_number, n = .)) %>% set_names(c('nlag1', 'nlag2', 'nlag3'))) %>% 
-  cbind(
-    map_dfc(
-      1:3,
-      ~ lead(comp_data$mean_mass, n = .)) %>% set_names(c('mlead1', 'mlead2', 'mlead3'))) %>% 
-  cbind(
-    map_dfc(
-      1:3,
-      ~ lag(comp_data$mean_mass, n = .)) %>% set_names(c('mlag1', 'mlag2', 'mlag3')))
+temp <- 
+  comp_data %>% 
+  group_by(site, year) %>% 
+  mutate(
+    nlead1 = lead(mean_number, 1),
+    nlead2 = lead(mean_number, 2),
+    nlead3 = lead(mean_number, 3),
+    nlag1 = lag(mean_number, 1),
+    nlag2 = lag(mean_number, 2),
+    nlag3 = lag(mean_number, 3),
+    mlead1 = lead(mean_mass, 1),
+    mlead2 = lead(mean_mass, 2),
+    mlead3 = lead(mean_mass, 3),
+    mlag1 = lag(mean_mass, 1),
+    mlag2 = lag(mean_mass, 2),
+    mlag3 = lag(mean_mass, 3)) %>% 
+  filter(year == 2021, site == 'NC Botanical Garden') %>% 
+  ungroup()
 
-# get R2 values for all - must group by year and site going into lm
+temp %>% 
+  select(mean_mass, starts_with('ml')) %>% 
+  map(~lm(temp$meanBiomass ~ .x, data =)) %>% 
+  map(summary) %>% 
+  map('r.squared') %>% 
+  bind_cols()
+
+# may need two separate map functions, one for each site, to account for missing years in PR
+
+map(
+  c('Prairie Ridge Ecostation', 'NC Botanical Garden'),
+  function(x){
+    temp <- 
+    comp_data %>% 
+    group_by(site, year) %>% 
+    mutate(
+      nlead1 = lead(mean_number, 1),
+      nlead2 = lead(mean_number, 2),
+      nlead3 = lead(mean_number, 3),
+      nlag1 = lag(mean_number, 1),
+      nlag2 = lag(mean_number, 2),
+      nlag3 = lag(mean_number, 3),
+      mlead1 = lead(mean_mass, 1),
+      mlead2 = lead(mean_mass, 2),
+      mlead3 = lead(mean_mass, 3),
+      mlag1 = lag(mean_mass, 1),
+      mlag2 = lag(mean_mass, 2),
+      mlag3 = lag(mean_mass, 3)) %>% 
+    filter(year == 2021, site == x) %>% 
+    ungroup()
+  
+  temp %>% 
+    select(mean_mass, starts_with('ml')) %>% 
+    map(~lm(temp$meanBiomass ~ .x, data =)) %>% 
+    map(summary) %>% 
+    map('r.squared') %>% 
+    bind_cols() %>% 
+    mutate(site = x, .before = 'mean_mass')
+}) %>% bind_rows()
+  
+comp_data %>% 
+  group_by(site, year) %>% 
+  mutate(
+    nlead1 = lead(mean_number, 1),
+    nlead2 = lead(mean_number, 2),
+    nlead3 = lead(mean_number, 3),
+    nlag1 = lag(mean_number, 1),
+    nlag2 = lag(mean_number, 2),
+    nlag3 = lag(mean_number, 3),
+    mlead1 = lead(mean_mass, 1),
+    mlead2 = lead(mean_mass, 2),
+    mlead3 = lead(mean_mass, 3),
+    mlag1 = lag(mean_mass, 1),
+    mlag2 = lag(mean_mass, 2),
+    mlag3 = lag(mean_mass, 3)) %>%  
+  group_by(site, year) %>% 
+  summarize(
+    biomass_mass = tryCatch(
+      summary(lm(meanBiomass ~ mean_mass))$r.squared %>% round(4),
+      error = function(c){return(0)})
+  ) %>% View()
